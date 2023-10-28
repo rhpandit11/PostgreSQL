59f6fcad56f1d3af9d014f127257d2f916254396
rhpandit11 <rh.pandit11@gmail.com>
2023-10-28 07:33:58 +0530

    update

--- managing_tables_&_databases.sql
+++ managing_tables_&_databases.sql
@@ -1,3 +1,5 @@
+-- How To create a TABLE with column
+
 CREATE TABLE director (
 director_id SERIAL PRIMARY KEY,
 director_account_name VARCHAR(20) UNIQUE,
@@ -5,3 +7,30 @@ first_name VARCHAR(50),
 last_name VARCHAR(50) DEFAULT 'Not Specified',
 date_of_birth DATE,
 address_id INT REFERENCES address(address_id))
+
+
+-- Challenge
+/*
+ALTER TABLE STEPS
+
+1.director_account_name to VARCHAR(30)
+2.drop the default on last_name
+3.add the constraint not null to last_name
+4.add the column email of data type VARCHAR(40)
+5.rename the director_account_name to account_name
+6.rename the table from director to directors
+
+*/
+
+ALTER TABLE director
+ALTER COLUMN director_account_name TYPE VARCHAR(30),
+ALTER COLUMN last_name DROP DEFAULT,
+ALTER COLUMN last_name SET NOT NULL,
+ADD COLUMN email VARCHAR(40)
+
+-----------------------------------------------------
+ALTER TABLE director
+RENAME director_account_name TO account_name
+
+ALTER TABLE director
+RENAME TO directors

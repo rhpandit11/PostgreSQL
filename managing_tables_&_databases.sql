a1fd648fcd70fa1e70973241106a09cf1e6c9e94
rhpandit11 <rh.pandit11@gmail.com>
2023-10-28 09:03:12 +0530

    update

--- managing_tables_&_databases.sql
+++ managing_tables_&_databases.sql
@@ -61,3 +61,34 @@ SELECT * FROM emp_table
 
 -- Truncate table
 TRUNCATE TABLE emp_table
+
+
+---Check
+/*
+Create a table called songs with the following columns:
+1. During creation add the DEFAULT 'Not defined' to the genre.
+2. Add the not null constraint to the song_name column
+3. Add the constraint with default name to ensure the price is at least 1.99.
+4. Add the constraint date_check to ensure the release date is between today and
+01-01-1950.
+5. Try to insert a row like this:
+6. Modify the constraint to be able to have 0.99 allowed as the lowest possible price.
+7. Try again to insert the row. 
+*/
+
+CREATE TABLE songs (
+	song_id SERIAL PRIMARY KEY,
+	song_name VARCHAR(30) NOT NULL,
+	genre VARCHAR(30) DEFAULT 'Not Defined',
+	price numeric(4,2) CHECK(price>=1.99),
+	release_date DATE CONSTRAINT CHECK(release_date BETWEEN '01-01-1950' AND CURRENT_DATE)
+)
+
+INSERT INTO songs(song_name, price, release_date)
+VALUES ('SQL song',0.99,'01-07-2023')
+
+ALTER TABLE songs
+DROP CONSTRAINT songs_price_check;
+
+ALTER TABLE songs
+ADD CONSTRAINT songs_price_check CHECK(price>=0.99)

2355f383a85bba2a8341467a13cf61b123ffebbc
rhpandit11 <rh.pandit11@gmail.com>
2023-11-02 17:28:06 +0530

    update

--- views_and_data_manipulation.sql
+++ views_and_data_manipulation.sql
@@ -94,3 +94,60 @@ ORDER BY length DESC
 
 --Refresh a materialized view
 REFRESH MATERIALIZED VIEW  mv_film_category
+
+/*
+In this challenge we use again the view v_customer_info lik this first:
+
+CREATE VIEW v_customer_info
+AS
+SELECT cu.customer_id,
+    cu.first_name || ' ' || cu.last_name AS name,
+    a.address,
+    a.postal_code,
+    a.phone,
+    city.city,
+    country.country
+     FROM customer cu
+     JOIN address a ON cu.address_id = a.address_id
+     JOIN city ON a.city_id = city.city_id
+     JOIN country ON city.country_id = country.country_id
+ORDER BY customer_id
+
+You need to perform the following tasks:
+
+1) Rename the view to v_customer_information.
+
+2) Rename the customer_id column to c_id.
+
+3) Add also the initial column as the third column to the view by replacing the view.
+*/
+
+--1) Rename the view to v_customer_information.
+
+ALTER VIEW v_customer_info
+RENAME TO v_customer_information
+
+
+--2) Rename the customer_id column to c_id.
+
+ALTER VIEW v_customer_information
+RENAME COLUMN customer_id TO c_id
+
+
+--3) Add also the initial column as the third column to the view by replacing the view.
+
+CREATE OR REPLACE VIEW v_customer_information
+AS
+SELECT cu.customer_id,
+    cu.first_name || ' ' || cu.last_name AS name,
+    cu.initials,
+	a.address,
+    a.postal_code,
+    a.phone,
+    city.city,
+    country.country
+     FROM customer cu
+     JOIN address a ON cu.address_id = a.address_id
+     JOIN city ON a.city_id = city.city_id
+     JOIN country ON city.country_id = country.country_id
+ORDER BY customer_id

f04000e74211ca9e03e6afd953e854ba3feebcc3
rhpandit11 <rh.pandit11@gmail.com>
2023-12-12 18:43:31 +0530

    update

--- Window_Functions/GROUPING_SETS/self_join.sql
+++ Window_Functions/GROUPING_SETS/self_join.sql
@@ -8,3 +8,19 @@ LEFT JOIN employee mng
 ON emp.manager_id=mng.employee_id
 LEFT JOIN employee mng2
 ON mng.manager_id=mng2.employee_id
+
+
+-----------------------------------
+
+--find all the pairs of films with the same length
+
+SELECT 
+f1.title,
+f2.title,
+f2.length
+FROM film f1
+LEFT JOIN film f2
+ON f1.length=f2.length
+WHERE
+f1.title<>f2.title
+ORDER BY length DESC
